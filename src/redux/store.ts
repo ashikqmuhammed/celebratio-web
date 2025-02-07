959378fdc2ba964a2951c847e018f81ce83180f4
Muhammed Ashik <ashikqmuhammed@gmail.com>
2025-02-07 19:27:17 +0530

    chore: configure Redux store

---
+++ src/redux/store.ts
@@ -0,0 +1,14 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+export const store=configureStore({
+    reducer:{
+       // [eventsApi.reducerPath]: eventsApi.reducer, // RTK Query Reducer
+      // events: eventsReducer, // Async thunk reducer
+    //   middleware: (getDefaultMiddleware) =>
+    //     getDefaultMiddleware().concat(eventsApi.middleware),
+    
+    }
+})
+
+export type RootState=ReturnType<typeof store.getState>;
+export type AppDispatch=typeof store.dispatch;
